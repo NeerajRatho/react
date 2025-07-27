91c6b83d5e9ea35c9cbeb17a11b28ba720f84b6a
NeerajRatho <neerajrathor970@gmail.com>
2025-07-27 22:45:22 +0530

    Added Tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState, useCallback, useEffect, useRef } from 'react'
 import './App.css'
+import '../dist/output.css'; // Adjust the path as needed
+
 
 function App() {
   const [length, setLength] = useState(8)
@@ -36,7 +38,7 @@ function App() {
   }, [length, numberAllowed, charAllowed, passwordGenerator])
 
   return (
-    <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gry-800 text-orange-500'>
+    <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500'>
       <h1 className='text-white text-center my-3'>Password generator</h1>
       <div className='flex shadow rounded-lg overflow-hidden mb-4'>
         <input
